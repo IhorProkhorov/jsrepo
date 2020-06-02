719bb85985879bcf9611630e09746da6f1dc8c02
Igor <example@gmail.com>
2020-06-02 22:17:26 +0300

    changed first.js

--- firstProject/first.js
+++ firstProject/first.js
@@ -11,16 +11,29 @@ const personalMovieDB = {
 };
 
 
-for(let i = 0; i <2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его', '');
-        if( a != null && b != null && a.length <= 50 && a != '' && b != ''  ){ 
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
+// for(let i = 0; i <2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его', '');
+//         if( a != null && b != null && a.length <= 50 && a != '' && b != ''  ){ 
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+// }
+let i = 0;
+while( i < 2){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его', '');
+            if( a != null && b != null && a.length <= 50 && a != '' && b != ''  ){ 
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+            i++;
 }
 
 if (personalMovieDB.count < 10){
